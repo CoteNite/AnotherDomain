a9132c2f031fb5a54f8878203788a647aacc8022
RRRRRRichardYoung <3399788235@qq.com>
2025-03-15 02:54:39 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+
+rootProject.name = "AnotherDomain"
+include("auth")
+include("commons")
