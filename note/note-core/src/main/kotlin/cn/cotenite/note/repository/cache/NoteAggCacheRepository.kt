216b49a3c6afc03e1fb30f0bab81596857a5e17a
RRRRRRichardYoung <3399788235@qq.com>
2025-05-15 04:49:51 +0800

    feat:Note的二级缓存

--- note/note-core/src/main/kotlin/cn/cotenite/note/repository/cache/NoteAggCacheRepository.kt
+++ note/note-core/src/main/kotlin/cn/cotenite/note/repository/cache/NoteAggCacheRepository.kt
@@ -1,5 +1,7 @@
 package cn.cotenite.note.repository.cache
 
+import cn.cotenite.asp.Slf4j
+import cn.cotenite.asp.Slf4j.Companion.log
 import cn.cotenite.note.common.utils.RedisKeyCreator
 import cn.cotenite.note.model.domain.NoteAgg
 import cn.cotenite.note.model.entity.dto.NoteDetailView
@@ -16,6 +18,7 @@ import java.util.concurrent.TimeUnit
  * @Description
  * @Date  2025/5/15 02:59
  */
+@Slf4j
 @Repository
 class NoteAggCacheRepository(
     private val redissonClient: RedissonClient,
@@ -33,13 +36,43 @@ class NoteAggCacheRepository(
 
     fun getNoteDetailCache(id: Long): NoteDetailView? {
         var agg = LOCAL_CACHE.getIfPresent(id)
-        agg?.let { return it.createNoteDetailView() }
-
-        val bucket = redissonClient.getBucket<NoteAgg>(RedisKeyCreator.noteDetailCacheKey(id))
-        agg = bucket.get()?:return null
-        threadPoolTaskExecutor.execute{
-            LOCAL_CACHE.put(id, agg)
+        if (agg != null) {
+            log.debug("命中本地缓存, id: {}", id)
+            return agg.createNoteDetailView()
         }
+
+        val bucket = redissonClient.getBucket<NoteAgg?>(RedisKeyCreator.noteDetailCacheKey(id))
+        try {
+            agg = bucket.get()
+        } catch (e: Exception) {
+            log.error("从 Redis 获取缓存失败, id: {}", id, e)
+            return null
+        }
+
+        if (agg == null) {
+            log.debug("Redis 缓存未命中或为 null, id: {}", id)
+            threadPoolTaskExecutor.execute {
+                try {
+                    val expirationSeconds = 60 + RandomUtil.randomLong(60)
+                    bucket.set(null, Duration.ofSeconds(expirationSeconds))
+                    log.debug("为不存在的 key 设置短时效 null 值成功, id: {}, 过期时间: {}s", id, expirationSeconds)
+                } catch (e: Exception) {
+                    log.error("为不存在的 key 设置短时效 null 值失败, id: {}", id, e)
+                }
+            }
+            return null
+        }
+
+        log.debug("Redis 缓存命中, id: {}", id)
+        threadPoolTaskExecutor.execute {
+            try {
+                LOCAL_CACHE.put(id, agg)
+                log.debug("异步更新本地缓存成功, id: {}", id)
+            } catch (e: Exception) {
+                log.error("异步更新本地缓存失败, id: {}", id, e)
+            }
+        }
+
         val view = agg.createNoteDetailView()
         return view
     }
